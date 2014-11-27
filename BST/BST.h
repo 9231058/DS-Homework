0e86cd7b0f4c471679e54fe32dee2da29b5a55e6
Parham Alvani <parham.alvani@gmail.com>
2014-11-27 10:03:53 +0330

    find sup is added to BST.
    sup(x) mean least element that larger than x.

--- BST/BST.h
+++ BST/BST.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <cstdlib>
+#include <stdexcept>
 #include <iostream>
 
-template<class T>
+template<typename T>
 class BST{
 	private:
 		class Node{
@@ -11,6 +12,7 @@ class BST{
 				T object;
 				Node* right;
 				Node* left;
+				Node* parent;
 			public:
 				Node(const T& object);
 
@@ -20,6 +22,8 @@ class BST{
 				Node* getRight();
 				void setLeft(Node* left);
 				Node* getLeft();
+				void setParent(Node* parent);
+				Node* getParent();
 		};
 		Node* root;
 
@@ -27,60 +31,74 @@ class BST{
 	public:
 		BST();
 
+		void remove(const T& object);
 		void insert(const T& object);
+		const T& sup(const T& object);
 		void inorder();
 };
 
-template<class T>
+template<typename T>
 BST<T>::Node::Node(const T& object){
 	this->object = T(object);
 	right = NULL;
 	left = NULL;
+	parent = NULL;
 }
 
-template<class T>
+template<typename T>
 void BST<T>::Node::setObject(const T& object){
 	this->object = T(object);
 }
 
-template<class T>
+template<typename T>
 T& BST<T>::Node::getObject(){
 	return this->object;
 }
 
-template<class T>
+template<typename T>
 void BST<T>::Node::setRight(Node* right){
 	this->right = right;
 }
 
-template<class T>
+template<typename T>
 typename BST<T>::Node* BST<T>::Node::getRight(){
 	return right;
 }
 
-template<class T>
+template<typename T>
 void BST<T>::Node::setLeft(Node* left){
 	this->left = left;
 }
 
-template<class T>
+template<typename T>
 typename BST<T>::Node* BST<T>::Node::getLeft(){
 	return left;
 }
 
-template<class T>
+template<typename T>
+void BST<T>::Node::setParent(Node* parent){
+	this->parent = parent;
+}
+
+template<typename T>
+typename BST<T>::Node* BST<T>::Node::getParent(){
+	return parent;
+}
+
+template<typename T>
 BST<T>::BST(){
 	root = NULL;
 }
 
-template<class T>
+template<typename T>
 void BST<T>::insert(const T& object){
 	if(root == NULL){
 		root = new Node(object);
 		return;
 	}
+	Node* new_node = new Node(object);
 	Node* x = root;
-	Node* y;
+	Node* y = NULL;
 	while(x != NULL){
 		y = x;
 		if(x->getObject() < object){
@@ -90,19 +108,84 @@ void BST<T>::insert(const T& object){
 		}
 	}
 	if(y->getObject() < object){
-		y->setRight(new Node(object));
+		y->setRight(new_node);
+		new_node->setParent(y);
 	}
 	if(y->getObject() > object){
-		y->setLeft(new Node(object));
+		y->setLeft(new_node);
+		new_node->setParent(y);
 	}
 }
 
-template<class T>
+template<typename T>
+const T& BST<T>::sup(const T& object){
+	if(root == NULL) throw std::invalid_argument("BST is empty, Cannot find your elemet");	
+
+	Node* x = root;
+	Node* y = NULL;
+	while(x != NULL){
+		y = x;
+		if(x->getObject() < object){
+			x = x->getRight();
+		}else if(x->getObject() == object){
+			break;
+		}
+		else{
+			x = x->getLeft();
+		}
+	}
+	x = NULL;
+	if(y->getObject() == object){
+		if(y->getRight() == NULL){
+			do{
+			x = y;
+			y = y->getParent();
+			}while(x == y->getRight());
+		}else{
+			x = y->getRight();
+			while(x != NULL){
+				y = x;
+				x = x->getLeft();
+			}
+		}
+	}else{
+		throw std::invalid_argument("404 Not Found");
+	}
+	if(y != NULL){
+		return y->getObject();
+	}else{
+		throw std::invalid_argument("404 Not Found");
+	}
+
+}
+
+template<typename T>
+void BST<T>::remove(const T& object){
+	if(root == NULL) throw std::invalid_argument("BST is empty, Cannot delete.");
+	
+	Node* x = root;
+	Node* y = NULL;
+	while(x != NULL){
+		y = x;
+		if(x->getObject() < object){
+			x = x->getRight();
+		}else{
+			x = x->getLeft();
+		}
+	}
+	if(y->getObject() == object){
+		if(!y->getRight() || !y->getLeft()){
+			
+		}
+	}
+}
+
+template<typename T>
 void BST<T>::inorder(){
 	inorder(root);
 }
 
-template<class T>
+template<typename T>
 void BST<T>::inorder(typename BST<T>::Node* start){
 	if(start){
 		inorder(start->getLeft());
--- BST/main.cpp
+++ BST/main.cpp
@@ -1,10 +1,19 @@
 #include <cstdlib>
+#include <iostream>
 #include "BST.h"
+
+using namespace std;
 
 int main(int argc, char* argv[]){
 	BST<int> ints;
 	ints.insert(10);
 	ints.insert(20);
 	ints.insert(5);
+	ints.insert(100);
+	ints.insert(12);
+	ints.insert(1);
+	ints.insert(32);
+	ints.insert(2);
 	ints.inorder();
+	cout << ints.sup(2) << endl;
 }
