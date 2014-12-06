d61425d04ae49badb6136fe779defc1cabb50761
Parham Alvani <parham.alvani@gmail.com>
2014-12-06 17:21:10 +0330

    Some mirror change in BST.

--- BST/BST.h
+++ BST/BST.h
@@ -4,7 +4,7 @@
 #include <stdexcept>
 #include <iostream>
 
-template<typename T>
+template<class T>
 class BST{
 	private:
 		class Node{
@@ -37,7 +37,7 @@ class BST{
 		void inorder();
 };
 
-template<typename T>
+template<class T>
 BST<T>::Node::Node(const T& object){
 	this->object = T(object);
 	right = NULL;
@@ -45,52 +45,52 @@ BST<T>::Node::Node(const T& object){
 	parent = NULL;
 }
 
-template<typename T>
+template<class T>
 void BST<T>::Node::setObject(const T& object){
 	this->object = T(object);
 }
 
-template<typename T>
+template<class T>
 T& BST<T>::Node::getObject(){
 	return this->object;
 }
 
-template<typename T>
+template<class T>
 void BST<T>::Node::setRight(Node* right){
 	this->right = right;
 }
 
-template<typename T>
+template<class T>
 typename BST<T>::Node* BST<T>::Node::getRight(){
 	return right;
 }
 
-template<typename T>
+template<class T>
 void BST<T>::Node::setLeft(Node* left){
 	this->left = left;
 }
 
-template<typename T>
+template<class T>
 typename BST<T>::Node* BST<T>::Node::getLeft(){
 	return left;
 }
 
-template<typename T>
+template<class T>
 void BST<T>::Node::setParent(Node* parent){
 	this->parent = parent;
 }
 
-template<typename T>
+template<class T>
 typename BST<T>::Node* BST<T>::Node::getParent(){
 	return parent;
 }
 
-template<typename T>
+template<class T>
 BST<T>::BST(){
 	root = NULL;
 }
 
-template<typename T>
+template<class T>
 void BST<T>::insert(const T& object){
 	if(root == NULL){
 		root = new Node(object);
@@ -117,7 +117,7 @@ void BST<T>::insert(const T& object){
 	}
 }
 
-template<typename T>
+template<class T>
 const T& BST<T>::sup(const T& object){
 	if(root == NULL) throw std::invalid_argument("BST is empty, Cannot find your elemet");	
 
@@ -159,7 +159,7 @@ const T& BST<T>::sup(const T& object){
 
 }
 
-template<typename T>
+template<class T>
 void BST<T>::remove(const T& object){
 	if(root == NULL) throw std::invalid_argument("BST is empty, Cannot delete.");
 	
@@ -180,12 +180,12 @@ void BST<T>::remove(const T& object){
 	}
 }
 
-template<typename T>
+template<class T>
 void BST<T>::inorder(){
 	inorder(root);
 }
 
-template<typename T>
+template<class T>
 void BST<T>::inorder(typename BST<T>::Node* start){
 	if(start){
 		inorder(start->getLeft());
